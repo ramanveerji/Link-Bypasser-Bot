4ecbecfd7ce1036a86166d402e1757e2ffc89746
RamandeepS <70764724+ramanveerji@users.noreply.github.com>
2023-08-30 17:13:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -110,10 +110,10 @@ def loopthread(message,otherss=False):
 # start command
 @app.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
+    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
     reply_markup=InlineKeyboardMarkup([
-        [ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Link-Bypasser-Bot")],
-        [ InlineKeyboardButton("Replit", url="https://replit.com/@bipinkrish/Link-Bypasser#app.py") ]]), 
+        [ InlineKeyboardButton("🌐 Our Channel", url="https://telegram.me/rs_bro")],
+        [ InlineKeyboardButton("Contact Admin", url="https://telegram.me/rs_m_bot") ]]), 
         reply_to_message_id=message.id)
 
 
